3c4b5aaca184a8b57c4943b5b0e4a7d117f61222
Johhed93 <jhedlundbergqvist@gmail.com>
2024-10-07 20:14:28 +0200

    added context for paths, toastmsgs

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,52 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './Pages/Home'
-import Navbar from './components/Navbar'
-import { Excercise } from './types/Excercise'
-import MyPage from './Pages/MyPage'
-import { useEffect, useState } from 'react'
-import Excercises from './Pages/ExcercisePage'
-import ExcerciseContext from './context/ExcerciseContext'
-import MyWorkoutContext from './context/myWorkoutcontext'
-import { WorkoutType } from './types/WorkoutType'
-const App=()=> {
-  const [myWorkouts, setMyWorkouts]= useState<WorkoutType[]>([])
-  const [excercises,setExcercises]=useState<Excercise[]>([]);
-  useEffect(()=>{
-  fetch("../excercise.json")
-  .then(res=>res.json())
-  .then(data=>setExcercises(data))
-  },[])
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import Navbar from "./components/Navbar";
+import { Excercise } from "./types/Excercise";
+import MyPage from "./Pages/MyPage";
+import { useEffect, useState } from "react";
+import Excercises from "./Pages/ExcercisePage";
+import ExcerciseContext from "./context/ExcerciseContext";
+import MyWorkoutContext from "./context/myWorkoutcontext";
+import { WorkoutType } from "./types/WorkoutType";
+import { ToastContainer } from "react-toastify";
+const App = () => {
+  const [myWorkouts, setMyWorkouts] = useState<WorkoutType[]>([]);
+  const [excercises, setExcercises] = useState<Excercise[]>([]);
+  useEffect(() => {
+    fetch("../excercise.json")
+      .then((res) => res.json())
+      .then((data) => setExcercises(data));
+  }, []);
   return (
     <>
-    <ExcerciseContext.Provider value={{excercises}}>
-     <BrowserRouter>
-     <Navbar/>
-     <Routes>
-      
-      <Route path='/' element={
-        <MyWorkoutContext.Provider value={{myWorkouts, setMyWorkouts}}>
-        <Home/>
-        </MyWorkoutContext.Provider>
-    }/>
-      <Route path='/myPage' element={<MyWorkoutContext.Provider value={{myWorkouts, setMyWorkouts}}>
-        <MyPage/>
-        </MyWorkoutContext.Provider>}/>
-     
-      <Route path='/excercises' element={<Excercises/>}/>
-     </Routes>
-     </BrowserRouter>
-     </ExcerciseContext.Provider>
+      <ToastContainer theme="dark" position="top-center" autoClose={5000} />
+      <ExcerciseContext.Provider value={{ excercises }}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <MyWorkoutContext.Provider value={{ myWorkouts, setMyWorkouts }}>
+                  <Home />
+                </MyWorkoutContext.Provider>
+              }
+            />
+            <Route path="/myPage" element={<MyPage />} />
+            <Route
+              path="/excercises"
+              element={
+                <MyWorkoutContext.Provider value={{ myWorkouts, setMyWorkouts }}>
+                  <Excercises />
+                </MyWorkoutContext.Provider>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </ExcerciseContext.Provider>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
