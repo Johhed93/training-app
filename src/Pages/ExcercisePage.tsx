1ade85f8dc259d6b232210e12a420087179098c2
Johhed93 <jhedlundbergqvist@gmail.com>
2024-10-08 20:37:39 +0200

    tsx for myWorkoutpage not done

--- src/Pages/ExcercisePage.tsx
+++ src/Pages/ExcercisePage.tsx
@@ -1,6 +1,19 @@
-const MyWorkoutPage=()=>{
-return(
-<><p>Excercise</p></>
-)
-}
-export default MyWorkoutPage
+import "../App.css";
+import MyWorkoutList from "../components/MyWorkoutList";
+
+const MyWorkoutPage = () => {
+  
+  return (
+    <div className="root-wrapper">
+      <div className="sidebar"></div>
+      <div className="central-content">
+        <h1 className="text-indigo-300 text-3xl mt-2">Mina Treningspass</h1>
+        <div className="excercises-container">
+        {<MyWorkoutList />}
+        </div>
+       
+        </div>
+    </div>
+  );
+};
+export default MyWorkoutPage;
