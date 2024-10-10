13e5332a6929000a0a7762c2ea3e966a5b2d2485
Johhed93 <jhedlundbergqvist@gmail.com>
2024-10-10 14:07:06 +0200

    added workoutcomponent

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,11 @@ import MyWorkoutContext from "./context/myWorkoutcontext";
 import { WorkoutType } from "./types/WorkoutType";
 import { ToastContainer } from "react-toastify";
 import IdContext from "./context/IdContext";
+import StartWorkout from "./Pages/StartWorkout";
 const App = () => {
   const [myWorkouts, setMyWorkouts] = useState<WorkoutType[]>([]);
   const [excercises, setExcercises] = useState<Excercise[]>([]);
-  const [id, SetId]=useState<number>(1)
+  const [id, SetId] = useState<number>(1);
   useEffect(() => {
     fetch("../excercise.json")
       .then((res) => res.json())
@@ -23,31 +24,39 @@ const App = () => {
   return (
     <>
       <ToastContainer theme="dark" position="top-center" autoClose={5000} />
-      <IdContext.Provider value={{id,SetId}}>
-      <ExcerciseContext.Provider value={{ excercises }}>
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <MyWorkoutContext.Provider value={{ myWorkouts, setMyWorkouts }}>
-                  <Home />
-                </MyWorkoutContext.Provider>
-              }
-            />
-            <Route path="/myPage" element={<MyPage />} />
-            <Route
-              path="/excercises"
-              element={
-                <MyWorkoutContext.Provider value={{ myWorkouts, setMyWorkouts }}>
-                  <Excercises />
-                </MyWorkoutContext.Provider>
-              }
-            />
-          </Routes>
-        </BrowserRouter>
-      </ExcerciseContext.Provider>
+      <IdContext.Provider value={{ id, SetId }}>
+        <ExcerciseContext.Provider value={{ excercises }}>
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <MyWorkoutContext.Provider value={{ myWorkouts, setMyWorkouts }}>
+                    <Home />
+                  </MyWorkoutContext.Provider>
+                }
+              />
+              <Route path="/myPage" element={<MyPage />} />
+              <Route
+                path="/excercises"
+                element={
+                  <MyWorkoutContext.Provider value={{ myWorkouts, setMyWorkouts }}>
+                    <Excercises />
+                  </MyWorkoutContext.Provider>
+                }
+              />
+              <Route
+                path="/startworkout/:id"
+                element={
+                  <MyWorkoutContext.Provider value={{ myWorkouts, setMyWorkouts }}>
+                    <StartWorkout />
+                  </MyWorkoutContext.Provider>
+                }
+              />
+            </Routes>
+          </BrowserRouter>
+        </ExcerciseContext.Provider>
       </IdContext.Provider>
     </>
   );
