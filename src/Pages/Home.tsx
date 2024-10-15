e82476f377eaff13b7c4c9ae4c3f52191cd4011d
Johhed93 <jhedlundbergqvist@gmail.com>
2024-10-15 22:05:39 +0200

    prettier and tailwind css

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -16,7 +16,6 @@ const Home = () => {
   const [muscleGroups, setMusclegroups] = useState<Excercise[]>([]);
   const [themes, setThemes] = useState<MuscleTheme[]>([]);
   const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
-  
 
   useEffect(() => {
     setMusclegroups(excercersiseContext.excercises);
@@ -36,32 +35,29 @@ const Home = () => {
     setMusclegroups(updatedlist);
     setSelectedTheme(musclegroup);
   };
-  
+
   return (
     <div className="root-wrapper">
-      
       <SaveWorkout
         selectedWorkout={selectedWorkout}
         setModalIsOpen={setModalIsOpen}
         modalIsOpen={modalIsOpen}
         setSelectedWorkout={setSelectedWorkout}
       />
-      <div className="sidebar">
-        <h2 className="theme-headline">Vad vill du träna?</h2>
-        {themes.map((theme, index) => (
-          <button
-            className="theme-button"
-            key={index}
-            onClick={() => handleButtonClick(theme.kroppsdel)}
-          >
-            {theme.kroppsdel}
-          </button>
-        ))}
-      </div>
-      <div className="central-content">
-        <h1 className="text-indigo-300 text-3xl mt-2">
-          Lägg till övningar, skapa ditt träningspass
-        </h1>
+      <div className="central-content bg-zinc-900">
+        <h1 className="text-indigo-300 text-3xl mt-2">Vad vill du träna?</h1>
+        <div className="flex gap-4">
+          {themes.map((theme, index) => (
+            <button
+              className="py-3 px-5 bg-zinc-700 rounded-md text-indigo-500 w-28 text-center text-lg hover:bg-zinc-800"
+              key={index}
+              onClick={() => handleButtonClick(theme.kroppsdel)}
+            >
+              {theme.kroppsdel}
+            </button>
+          ))}
+        </div>
+
         <h2 className="text-indigo-300 text-lg">
           {selectedTheme === "" ? "Alla övningar" : `Alla ${selectedTheme} övningar`}
         </h2>
@@ -73,8 +69,8 @@ const Home = () => {
           />
         </div>
       </div>
-      <div className="sidebar">
-        <h2 className="theme-headline">Mitt träningspass</h2>
+      <div className="sidebar bg-zinc-900">
+        <h2 className="theme-headline mt-48">Mitt träningspass</h2>
         <ul className="sidebar-list">
           {selectedWorkout.length === 0 ? (
             <li>Inga øvningar lagt till en</li>
