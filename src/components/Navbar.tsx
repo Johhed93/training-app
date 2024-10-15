ed5fe348e93ad950603777c859f09c3df0c4f34e
Johhed93 <jhedlundbergqvist@gmail.com>
2024-10-15 22:04:13 +0200

    made nav in tailwind

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,9 +21,9 @@ const navlinks:Navlinks[]=[
 ]
 
 return(
-<header className="flex justify-center items-center p-6 bg-black border-b-2 border-white">
-<nav className="flex justify-between items-center w-10/12 ">
-    <img src="https://cdn.pixabay.com/photo/2017/02/15/00/48/logo-2067396_1280.png" alt="" className="max-w-16" />
+<header className="flex justify-center items-center p-6 bg-zinc-900">
+<nav className="flex justify-between items-center w-11/12 ">
+    <div className="py-2 px-3.5 bg-zinc-700 text-xl text-indigo-300 rounded-md">Tränings app</div>
     <ul className="flex gap-x-3.5">{navlinks.map((link,index)=>(
         <li key={index}>
             <Link to={link.to} className="text-indigo-500 text-lg">{link.name}</Link>
